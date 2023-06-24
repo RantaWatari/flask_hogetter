a57ed969be1a72928e4d7e122d1f636201c0999e
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2023-06-24 18:03:01 +0900

    create,delete,updateのルートを追加

--- flask_file/hogetter.py
+++ flask_file/hogetter.py
@@ -7,3 +7,21 @@ bp = Blueprint("hogetter",__name__,url_prefix="/hogetter")
 @login_required
 def index():
     return render_template("index.html")
+
+
+@bp.route("/create",methods=["GET","POST"])
+@login_required
+def create():
+    return render_template("hogetter/hogetter_form.html",command="test")
+
+
+@bp.route("/delete",methods=["GET","POST"])
+@login_required
+def delete():
+    return render_template("hogetter/hogetter_form.html",command="test")
+
+
+@bp.route("/update",methods=["GET","POST"])
+@login_required
+def update():
+    return render_template("hogetter/hogetter_form.html",command="test")
