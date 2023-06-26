04f31ffb5e20f2ce5fccb182abdec20d19cedb17
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2023-06-26 17:26:47 +0900

    hogeetの生成と保存、保存されたhogeetのindexページでの表示までコーディングした。

--- flask_file/hogetter.py
+++ flask_file/hogetter.py
@@ -1,18 +1,22 @@
-from flask import Blueprint,render_template,request
+from flask import Blueprint,render_template,request,redirect,url_for
 from auth import login_required
+from hogetter_db import show_db,create_db
 
 bp = Blueprint("hogetter",__name__,url_prefix="/hogetter")
 
 @bp.route("/",methods=["GET"])
 @login_required
 def index():
-    return render_template("index.html")
+    return render_template("index.html",posts=show_db())
 
 
-@bp.route("/create",methods=["GET","POST"])
+@bp.route("/create",methods=["POST"])
 @login_required
 def create():
-    return render_template("hogetter/hogetter_form.html",command="test")
+    hogeet = request.form.get("hogeet")
+    create_db(hogeet)
+
+    return redirect(url_for("hogetter.index"))
 
 
 @bp.route("/delete",methods=["GET","POST"])
--- flask_file/hogetter_db.py
+++ flask_file/hogetter_db.py
@@ -6,8 +6,37 @@ def connect_db_text():
     return db
 
 
-def connect_db_text():
+def connect_db_drive():
     db = Deta(project_key="c0wmhhz3nbe_aT8g675zLGZKhcfQKzvn7CaBTU6oqSjg")
     db = db.Drive(name="hogetter_drive")
     return db
+
+
+
+def create_db(hogeet):
+    
+    connect_db_text().put({
+        "text":hogeet
+    })
+
+def show_db():
+    db = connect_db_text()
+    fetch_data = db.fetch()
+    
+    #(3)
+    try:  
+        ## Usefull only deploy enviroment
+        for i in fetch_data:
+            datas = i
+        posts = []
+        for j in datas:
+            posts.append(j)
+
+    except TypeError:
+        ## Usefull Only local enviroment
+        posts = fetch_data.items
+
+    return posts
+
+
 
