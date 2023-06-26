39d4986e79da9d678bde7429d45565a09bf5922a
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2023-06-26 20:48:28 +0900

    hogeetのdeleteやhogeetの作られた時間や作成者が分かるようにした。

--- flask_file/hogetter.py
+++ flask_file/hogetter.py
@@ -1,31 +1,37 @@
-from flask import Blueprint,render_template,request,redirect,url_for
+from flask import Blueprint,render_template,request,redirect,url_for,g
 from auth import login_required
-from hogetter_db import show_db,create_db
+from hogetter_db import show_db_text,create_db_text,delete_db_text
 
 bp = Blueprint("hogetter",__name__,url_prefix="/hogetter")
 
 @bp.route("/",methods=["GET"])
 @login_required
 def index():
-    return render_template("index.html",posts=show_db())
+
+    return render_template("index.html", posts = show_db_text(), owner = g.user)
 
 
 @bp.route("/create",methods=["POST"])
 @login_required
 def create():
     hogeet = request.form.get("hogeet")
-    create_db(hogeet)
+    create_db_text(hogeet)
 
     return redirect(url_for("hogetter.index"))
 
 
-@bp.route("/delete",methods=["GET","POST"])
+@bp.route("/<hogeet_id>/delete",methods=["GET"])
 @login_required
-def delete():
-    return render_template("hogetter/hogetter_form.html",command="test")
+def delete(hogeet_id):
+    delete_db_text(hogeet_id=hogeet_id)
+    
+    return redirect(url_for("hogetter.index"))
 
 
-@bp.route("/update",methods=["GET","POST"])
+@bp.route("/<hogeet_id>/update",methods=["GET","POST"])
 @login_required
 def update():
-    return render_template("hogetter/hogetter_form.html",command="test")
+    
+    pass
+    
+    return redirect(url_for("hogetter.index"))
--- flask_file/hogetter_db.py
+++ flask_file/hogetter_db.py
@@ -1,4 +1,6 @@
 from deta import Deta
+import datetime
+from flask import g
 
 def connect_db_text():
     db = Deta(project_key="c0wmhhz3nbe_tc1hZ2nHqv9o6f8rrVRqpADMqxL7jtjX")
@@ -11,15 +13,25 @@ def connect_db_drive():
     db = db.Drive(name="hogetter_drive")
     return db
 
-
-
-def create_db(hogeet):
+def now_time():
+    now_time = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))) 
+    now_time_tuple = now_time.timetuple()
+    now = []
     
-    connect_db_text().put({
-        "text":hogeet
-    })
+    for i in range(len(now_time_tuple[:6])):
+        time = str(now_time_tuple[i])
+        if i == 0 :
+            time = time.zfill(4)
+        else:
+            time = time.zfill(2)
+        now.append(time)
+    # year~microsecond 
+    time = (str(now_time.microsecond))
+    now.append(time.zfill(6))
+    return now
 
-def show_db():
+
+def show_db_text():
     db = connect_db_text()
     fetch_data = db.fetch()
     
@@ -39,4 +51,14 @@ def show_db():
     return posts
 
 
+def create_db_text(hogeet):
+    
+    connect_db_text().put({
+        "time":now_time(),
+        "owner":g.user,
+        "text":hogeet
+    })
 
+
+def delete_db_text(hogeet_id):
+    connect_db_text().delete(hogeet_id)
