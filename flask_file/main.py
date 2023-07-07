3c26d45b9327d2c4e5f3ea51bf84093018740dfa
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2023-07-07 09:33:40 +0900

    Deta.spaceの仕様やが少し変わったらしい。このコミット前の状態でデプロイしたときに訳の分からんerrorが頻発してコンテンツが表示できなかった。Deta.spaceのtutorialではgunicornを使っているが、flask runでも良いみたいなので現行はこのままでいく。しかし、一つだけ解決してない問題として、Deta.spaceの管理画面でlogが表示されない。他のlogもR.5.6..27あたりで更新が止まっている。

--- flask_file/main.py
+++ flask_file/main.py
@@ -1,21 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask
 import auth,hogetter
-from auth import login_required
 
 app = Flask(__name__)
 app.secret_key ="test"
 
-
-@app.route("/test",methods=["GET"])
-def test():
-    return "test"
-
-@app.route("/test/<string>",methods=["GET"])
-def test_str(string):
-    return f"Test {string}!!"
-
-
 app.register_blueprint(auth.bp)
 app.register_blueprint(hogetter.bp)
-
 
