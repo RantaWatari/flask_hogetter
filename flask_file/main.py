de522402c040e25f06a329179bdd49d51c29baeb
Ranta Watari <fishmanranbo_0829@docomo.ne.jp>
2023-08-18 17:18:13 +0900

    セキュリティ的に今のままでいいのか不安なので勉強している途中

--- flask_file/main.py
+++ flask_file/main.py
@@ -3,6 +3,7 @@ import auth,hogetter
 
 app = Flask(__name__)
 app.secret_key ="test"
+app.session_cookie_name="hoggeter_cookie"
 
 app.register_blueprint(auth.bp)
 app.register_blueprint(hogetter.bp)
