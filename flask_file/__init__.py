7bd6fcc8f10d1790ad47ccff7e9efc88b6989188
Ranta Watari <watariranta@gmail.com>
2024-05-02 23:09:10 +0900

    とりあえず、pytestが使えるようにFlaskアプリケーションの起動方法を変えてみた。公式ではApplication Factoriesと紹介されている。

--- flask_file/__init__.py
+++ flask_file/__init__.py
@@ -1,3 +1,17 @@
-from . import main
-def app():
-    main()
+from flask import Flask, redirect, url_for
+from flask_file import auth,hogetter
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key ="test"
+
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(hogetter.bp)
+    
+    @app.route("/")
+    def top():
+        return redirect(url_for("hogetter.index"))
+    
+    return app
+
+app = create_app()
